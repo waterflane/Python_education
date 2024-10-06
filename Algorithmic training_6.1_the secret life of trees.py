38727c1952cda5bdccfd67e2cdf5904acf80eb6c
waterflane <106469980+waterflane@users.noreply.github.com>
2024-10-06 14:55:30 +0300

    Deleted tests for Algorithmic training_6.1_the secret life of trees.py

--- Algorithmic training_6.1_the secret life of trees.py
+++
@@ -1,9 +0,0 @@
-def main(list_trees):
-    ...
-
-assert main([0]) == 1, main([0])
-assert main([0, 0, 3, 3]) == 1, main([0, 0, 3, 3])
-assert main([10]) == 10, main([10])
-assert main([5, 0, 3, 2, 1, 4]) == 0, main([5, 0, 3, 2, 1, 4])
-assert main([0,0,0,0,11]) == 7, main([0,0,0,0,11])
-
